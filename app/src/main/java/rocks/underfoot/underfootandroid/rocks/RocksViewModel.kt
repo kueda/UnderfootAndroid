4488761ba694a136b6a08aa6be007926ca831321
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2020-08-29 14:56:32 -0700

    Set current feature on map change; transform feature properties to view strings and use in bottom sheet

--- app/src/main/java/rocks/underfoot/underfootandroid/rocks/RocksViewModel.kt
+++ app/src/main/java/rocks/underfoot/underfootandroid/rocks/RocksViewModel.kt
@@ -7,12 +7,13 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.mapzen.tangram.*
+import kotlin.math.roundToInt
 
 class RocksViewModel : ViewModel(),
     MapController.SceneLoadListener,
-    TouchInput.RotateResponder,
     TouchInput.DoubleTapResponder,
-    TouchInput.ShoveResponder
+    MapChangeListener,
+    FeaturePickListener
 {
     private val TAG = "RocksViewModel"
     private val SCENE_FILE_PATH = "asset:///usgs-state-color-scene.yml"
@@ -42,29 +43,53 @@ class RocksViewModel : ViewModel(),
     private val zoom = MutableLiveData<Float>(10.0F)
     val zoomString: LiveData<String> = Transformations.map(zoom) {z -> "%.1f".format(z) }
 
-    // Note that in Java it seems like you can instantiate an interface like new MapChangeListener,
-    // but not in Kotlin. In the latter, this syntax works, though I'm not sure if it's conventional
-    private val mapChangeListener = object: MapChangeListener {
-        // Not sure why it thinks these need to be here even if nothing happens
-        override fun onViewComplete() {}
+    private var feature = MutableLiveData<FeaturePickResult>()
+    val featureTitle: LiveData<String> = Transformations.map(feature) {f ->
+        featurePropertyString("title", f.properties)
+    }
+    val featureLithology: LiveData<String> = Transformations.map(feature) {f ->
+        featurePropertyString("lithology", f.properties, default="Lithology: Unknown")
+    }
+    val featureDescription: LiveData<String> = Transformations.map(feature) {f ->
+        featurePropertyString("description", f.properties)
+    }
+    val featureAge: LiveData<String> = Transformations.map(feature) {f ->
+        val estAge = humanizeAge(f.properties["est_age"]);
+        val span = featurePropertyString("span", f.properties)
+            .capitalize().replace(" To ", " to ")
+        "Age: $span ($estAge)"
+    }
+    val featureEstAge: LiveData<String> = Transformations.map(feature) {f ->
+        val span = featurePropertyString("span", f.properties)
+            .capitalize().replace(" To ", " to ")
+        "$span (${humanizeAge(f.properties["max_age"])} - ${humanizeAge(f.properties["min_age"])})"
+    }
+    val featureSource: LiveData<String> = Transformations.map(feature) {f ->
+        featurePropertyString("source", f.properties)
+    }
 
-        override fun onRegionWillChange(animated: Boolean) {}
-
-        override fun onRegionIsChanging() {
-            // update the map metadata as you move the map
-            val p = mapController.cameraPosition
-            zoom.value = p.zoom
-            lat.value = p.position.latitude
-            lng.value = p.position.longitude
+    private fun humanizeAge(ageArg: String?): String {
+        val default = "?"
+        val age = if (ageArg.isNullOrEmpty()) return default else ageArg
+        val ageNum = age.toFloatOrNull() ?: return default
+        return when {
+            ageNum >= 1000000000 -> "%.1f Ga".format(ageNum / 1000000000.0)
+            ageNum >= 1000000 -> "%.1f Ma".format(ageNum / 1000000.0)
+            ageNum >= 100000 -> "%.1f ka".format(ageNum / 1000.0)
+            else -> "%,d years".format(ageNum.roundToInt())
         }
+    }
 
-        override fun onRegionDidChange(animated: Boolean) {
-            // update the map metadata when done moving, including on initial load
-            val p = mapController.cameraPosition
-            zoom.value = p.zoom
-            lat.value = p.position.latitude
-            lng.value = p.position.longitude
+    private fun featurePropertyString(
+        propName: String,
+        properties: Map<String, String>,
+        default: String = "Unknown"
+    ): String {
+        if (properties.isEmpty()) return default
+        properties[propName]?.let {
+            if (it.length > 0 ) return it
         }
+        return default
     }
 
     // Note that this isn't an override, it's just a way for the fragment to assign the
@@ -76,12 +101,23 @@ class RocksViewModel : ViewModel(),
         mapController.let {
             it.setSceneLoadListener(this)
             it.loadSceneFile(SCENE_FILE_PATH)
-            it.setMapChangeListener(mapChangeListener)
+            it.setMapChangeListener(this)
+            it.setFeaturePickListener(this)
             it.touchInput.let {ti ->
 //            ti.setTapResponder(this);
                 ti.setDoubleTapResponder(this);
-                ti.setRotateResponder(this);
-                ti.setShoveResponder(this)
+                ti.setRotateResponder(object: TouchInput.RotateResponder {
+                    // Disable rotation
+                    override fun onRotateBegin(): Boolean { return true }
+                    override fun onRotate(x: Float, y: Float, rotation: Float): Boolean { return true }
+                    override fun onRotateEnd(): Boolean { return true }
+                });
+                ti.setShoveResponder(object: TouchInput.ShoveResponder {
+                    // Disable perspective changes, though dang, how cool would it be to have a 3D DEM
+                    override fun onShoveBegin(): Boolean { return false }
+                    override fun onShove(distance: Float): Boolean { return true }
+                    override fun onShoveEnd(): Boolean { return false }
+                })
             }
         }
     }
@@ -97,11 +133,6 @@ class RocksViewModel : ViewModel(),
             CameraUpdateFactory.newLngLatZoom(pos, zoom.value!!), 500)
     }
 
-    // Disable rotation
-    override fun onRotateBegin(): Boolean { return true }
-    override fun onRotate(x: Float, y: Float, rotation: Float): Boolean { return true }
-    override fun onRotateEnd(): Boolean { return true }
-
     // Zoom on double tap
     override fun onDoubleTap(x: Float, y: Float): Boolean {
         val newZoom = mapController.cameraPosition.zoom + 1F;
@@ -112,8 +143,30 @@ class RocksViewModel : ViewModel(),
         return true;
     }
 
-    // Disable perspective changes, though dang, how cool would it be to have a 3D DEM
-    override fun onShoveBegin(): Boolean { return false }
-    override fun onShove(distance: Float): Boolean { return true }
-    override fun onShoveEnd(): Boolean { return false }
+    // MapChangeListener ovverrides
+    override fun onViewComplete() {}
+    override fun onRegionWillChange(animated: Boolean) {}
+    override fun onRegionIsChanging() {
+        // update the map metadata as you move the map
+        val p = mapController.cameraPosition
+        zoom.value = p.zoom
+        lat.value = p.position.latitude
+        lng.value = p.position.longitude
+    }
+    override fun onRegionDidChange(animated: Boolean) {
+        // update the map metadata when done moving, including on initial load
+        val p = mapController.cameraPosition
+        zoom.value = p.zoom
+        lat.value = p.position.latitude
+        lng.value = p.position.longitude
+        val center = mapController.lngLatToScreenPosition(p.position)
+        Log.d(TAG, "center: $center")
+        mapController.pickFeature(center.x, center.y)
+    }
+
+    override fun onFeaturePickComplete(result: FeaturePickResult?) {
+        Log.d(TAG, "onFeaturePickComplete, result: $result")
+        if (result == null) { return }
+        feature.value = result
+    }
 }
